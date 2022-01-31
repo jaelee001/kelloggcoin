0b6084e575c5258f2cbfba38eb1ff946b533fd21
jaelee001 <jaehee.lee@kellogg.northwestern.edu>
2022-01-31 03:52:16 +0000

    attempt_1

--- practice.rb
+++ practice.rb
@@ -27,3 +27,70 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+#outgoing fund array
+from_ben = []
+for block in blockchain
+  if block[:from_user] == "ben"
+    from_ben = from_ben + [block[:amount]]
+  end
+end
+
+from_brian = []
+for block in blockchain
+  if block[:from_user] == "brian"
+    from_brian = from_brian + [block[:amount]]
+  end
+end
+
+from_evan = []
+for block in blockchain
+  if block[:from_user] == "evan"
+    from_evan = from_evan + [block[:amount]]
+  end
+end
+
+from_anthony = []
+for block in blockchain
+  if block[:from_user] == "anthony"
+    from_anthony = from_anthony + [block[:amount]]
+  end
+end
+
+#incoming fund array
+to_ben = []
+for block in blockchain
+  if block[:to_user] == "ben"
+    to_ben = to_ben + [block[:amount]]
+  end
+end
+
+to_brian = []
+for block in blockchain
+  if block[:to_user] == "brian"
+    to_brian = to_brian + [block[:amount]]
+  end
+end
+
+to_evan = []
+for block in blockchain
+  if block[:to_user] == "evan"
+    to_evan = to_evan + [block[:amount]]
+  end
+end
+
+to_anthony = []
+for block in blockchain
+  if block[:to_user] == "anthony"
+    to_anthony = to_anthony + [block[:amount]]
+  end
+end
+
+#$$ per user
+ben = to_ben.sum - from_ben.sum
+brian = to_brian.sum - from_brian.sum
+evan = to_evan.sum - from_evan.sum
+anthony = to_anthony.sum - from_anthony.sum
+puts "Ben's KelloggCoin balance is #{ben}"
+puts "Brian's KelloggCoin balance is #{brian}"
+puts "Evan's KelloggCoin balance is #{evan}"
+puts "Anthony's KelloggCoin balance is #{anthony}"
